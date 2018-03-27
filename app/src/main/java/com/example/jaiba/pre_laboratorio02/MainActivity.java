0fa0381bf14ad1b15d7a728ed9bc9740d664b1af
DESKTOP-8E5E3T5\jaiba <jaibarra1@miuandes.cl>
2018-03-27 12:17:50 -0300

    Laboratorio 02

--- app/src/main/java/com/example/jaiba/pre_laboratorio02/MainActivity.java
+++ app/src/main/java/com/example/jaiba/pre_laboratorio02/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.jaiba.pre_laboratorio02;
 
+import android.app.Fragment;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, FormularioFragment.OnFragmentInteractionListener  {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -19,5 +30,52 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        boolean FragmentTran = false;
+        FormularioFragment fragmento = null;
+
+        if (id == R.id.formulario) {
+
+            fragmento = new FormularioFragment();
+            FragmentTran=true;
+
+        }
+        else if (id == R.id.settings) {
+
+        }
+        else if (id == R.id.help) {
+
+        }
+
+        if (FragmentTran){
+            getSupportFragmentManager().beginTransaction().replace(R.id.header,fragmento).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
